ac068c851b15b786c5bcbf3c09f36135b66cb623
Sienna Kopf <princess.kopf@gmail.com>
2020-05-10 22:50:31 -0600

    Add war game test file *incomplete*

--- test/war_game_test.rb
+++ test/war_game_test.rb
@@ -9,52 +9,26 @@ require './lib/war_game'
 class WarGameTest < Minitest::Test
 
   def test_it_exists
-    war_game = WarGame.new
+    game = WarGame.new
 
-    assert_instance_of WarGame, war_game
-  end
-
-  def test_create_a_52_card_deck
-    card_deck = Deck.new
-
-    assert_equal 52, card_deck.create_52_card_deck.count
-  end
-
-  def test_create_a_real_deck_with_unique_cards
-    skip
-    card_deck = Deck.new
-
-  end
-
-  def test_a_shuffled_deck_is_not_a_unshuffled_deck
-    card_deck_1 = Deck.new.create_52_card_deck
-    card_deck_2 = Deck.new.create_52_card_deck
-
-    shuffled_card_deck_1 = card_deck_1.shuffle
-    refute_equal shuffled_card_deck_1, card_deck_2
-
-    shuffled_card_deck_2 = card_deck_2.shuffle
-    refute_equal shuffled_card_deck_1, shuffled_card_deck_2
-  end
-
-
-  def test_split_deck_in_half
-    full_deck = Deck.new
-
-    split_deck = full_deck.split
-
-    assert_equal 26, split_deck[split_deck.keys[0]].count
-    assert_equal 26, split_deck[split_deck.keys[1]].count
-    assert_equal 'deck1', split_deck.key(split_deck.values[0])
-    assert_equal 'deck2', split_deck.key(split_deck.values[1])
-
-
+    assert_instance_of WarGame, game
   end
 
   def test_assign_split_deck_to_players
+    game = WarGame.new
+    game.setup
 
 
+    assert_equal 'CECE', game.player1.name
+    assert_equal 'CARLO', game.player2.name
+    assert_equal 26, game.player1.deck.count
+    assert_equal 26, game.player2.deck.count
   end
 
+  def test_mad_type_turn_works
+  #   game = WarGame.new
+  #   game.setup
+  #   game.start
+  end
 
 end
